bd82d0a42d0de83d3fb605d8545d89ac237dfe71
Jerry <jerry.clou@outlook.com>
2023-07-03 14:28:26 +0200

    creation de l'autoloader

---
+++ autoloader.php
@@ -0,0 +1,13 @@
+<?php
+
+class Autoload
+{
+    public static function inclusionAuto($className)
+    {
+        require_once __DIR__. '/'. str_replace('\\', DIRECTORY_SEPARATOR, $className). '.php';
+    }
+}
+spl_autoload_register('Autoload','inclusionAuto');
+
+
+// echo __DIR__;
