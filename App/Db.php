61ef40c7c8e656c551f11f2abc3be8387b512ce3
Jerry <jerry.clou@outlook.com>
2023-07-03 13:53:57 +0200

    1er Etape création de Db,confi.json

---
+++ App/Db.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+class Db
+{
+    private static $db;
+
+    static function getDb()
+    {
+        if(!sefl::$db)
+        {
+            try{
+                $config = file_get_contents('../App/config.json');
+                $congfig = json_decode($config);
+                self::$db = new PDO("mysql:host=".$config->host.";dbname=".$config->dbname, $config->username,$config->password);
+            }catch(PDOException $e){
+                echo 'le fichier pose problème, erreur : '.$e->getMessage();
+            }
+        }
+        return self::$db;
+    }
+}
+
+var_dump($config);
+
+
