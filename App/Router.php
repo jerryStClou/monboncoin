d31a14d21ccfb860c44b0cb655667fdcc93cfc94
Jerry <jerry.clou@outlook.com>
2023-07-04 16:52:12 +0200

    toutes les routes du site

---
+++ App/Router.php
@@ -0,0 +1,71 @@
+<?php
+namespace App;
+
+class Router
+{
+    public function app()
+    {
+         // on test le routeur
+        // echo "<br> le routeur fonctionne";
+       // Récupération de l'url
+       $request = $_SERVER["REQUEST_URI"];
+    //    echo "<pre>";
+    //    var_dump($request); 
+    //    echo "</pre>";
+
+      // On supprime tout le debut ("/monboncoin/public") de $request
+      $nb = strlen(SITEBASE);
+    //   echo $nb;
+      $request = substr($request, $nb);
+      echo "<br>";
+    //   echo $request;    
+      $request = explode("?",$request);
+      $request = $request[0];
+    //   echo $request;
+
+    //   on defini les routes du projet
+
+       switch ($request)
+       {
+        case '':
+            echo 'page d\'accueil affiche les deux dernières annonces';
+            break;
+        case 'annonces':
+            echo 'toutes les annonces';
+            break;
+        case 'annonceDetail':
+            echo 'affichage d\'une annonce';
+            break;
+        case 'annonceAjout':
+            echo 'ajout d\'une annonce';
+            break;
+        case 'annonceModif':
+            echo "modification d'une annonce";
+            break;
+        case 'annonceSupp':
+            echo "suppression d'une annonce";
+            break;
+        case 'annonceConfirm':
+            echo 'confirmation de suppression';
+            break;
+        case 'panier':
+            echo 'votre panier';
+            break;
+        case 'inscription':
+            echo 'inscription';
+            break;
+        case 'connexion':
+            echo 'connexion';
+            break;
+        case 'deconnexion':
+            echo 'deconnexion';
+            break;
+        case 'profil':
+            echo 'profil';
+            break;
+        default:
+            echo 'cette page n\'existe pas 404';
+            break;
+       }
+    }
+}
