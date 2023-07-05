bb867ab5ddaf0948831dc04d7080e32946846a7e
Jerry <jerry.clou@outlook.com>
2023-07-05 10:31:45 +0200

    1er etpe des vues

---
+++ Views/layout.php
@@ -0,0 +1,10 @@
+<?php
+require_once('header.php'); ?>
+
+
+<!-- contenu principal -->
+Je veux faire apparaitre les infos envoyé par le controller ici
+<?php echo $data ;?>
+
+
+<?php require_once('footer.php');?>
